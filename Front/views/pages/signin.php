e7353531dcf9274841cb192b54d1eb2c1d359dff
Guillaume Lebelt <guillaumelebelt@gmail.com>
2016-04-16 23:28:23 +0200

    Update signin page

---
+++ Front/views/pages/signin.php
@@ -0,0 +1,33 @@
+<div class="container ">
+    <div class="content">
+    	<div class="row">
+    		<div class="col-md-12 login_intro">
+    			<h2>
+        			Créez dès maintenant votre projet
+    				<br />
+    				Et consacrez-vous y pleinement !
+    			</h2>
+                <h3>Connection</h3>
+                <form class="login_form">
+
+                    <input type="text" class="login_username margin_login" required="true" placeholder="Prénom" />
+
+                    <input type="text" class="login_username margin_login" required="true" placeholder="Nom" />
+
+                    <input type="mail" class="login_username margin_login" required="true" placeholder="Mail" />
+
+                    <input type="password" class="login_password margin_login" required="true" placeholder="Mot de passe" />
+
+                     <input type="password" class="login_password margin_login" required="true" placeholder="Confirmation Mot de passe" />
+
+                    <input type="submit" name="Login" value="Inscription >" class="login_submit" />
+
+                    <a href="#">J'ai déjà un compte</a>
+                </form>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+</div> <!-- Container gradient end -->

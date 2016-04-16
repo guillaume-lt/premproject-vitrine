027fbb0e7e9564447a6b16bae3b556b80f903e2c
Guillaume Lebelt <guillaumelebelt@gmail.com>
2016-04-17 00:03:55 +0200

    Finish signin page

--- Front/views/pages/signin.php
+++ Front/views/pages/signin.php
@@ -8,7 +8,7 @@
     				Et consacrez-vous y pleinement !
     			</h2>
                 <h3>Inscription</h3>
-                <form class="login_form">
+                <form class="signin_form">
 
                     <input type="text" class="login_username margin_login" required="true" placeholder="Prénom" />
 
@@ -20,9 +20,14 @@
 
                      <input type="password" class="login_password margin_login" required="true" placeholder="Confirmation Mot de passe" />
 
+                     <div class="signin_checkbox">
+                        <input id="form_checkbox" type="checkbox" required="true"/>
+                        <label for="form_checkbox">J'accepte les conditions d'utilisations</label>
+                    </div>
+
                     <input type="submit" name="Login" value="Inscription >" class="login_submit" />
 
-                    <a href="#">J'ai déjà un compte</a>
+                    <a href="<?= URL ?>login">J'ai déjà un compte</a>
                 </form>
 			</div>
 		</div>
